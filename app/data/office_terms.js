56a7eebc9698066e946e923b34bd20ca04cb3675
SideNote <166878218+YaMac33@users.noreply.github.com>
2026-03-25 23:31:36 +0900

    Add office_terms.js with office term data

---
+++ app/data/office_terms.js
@@ -0,0 +1,39 @@
+window.APP_DATA = window.APP_DATA || {};
+window.APP_DATA.office_terms = [
+  {
+    office_term_id: "OT001",
+    member_id: "M001",
+    term_start_date: "2023-05-01",
+    term_end_date: "",
+    election_label: "2023年市議会議員一般選挙",
+    elected_count: 2,
+    end_reason_code: "",
+    note: "現職",
+    created_at: "2026-03-25",
+    updated_at: "2026-03-25"
+  },
+  {
+    office_term_id: "OT002",
+    member_id: "M002",
+    term_start_date: "2023-05-01",
+    term_end_date: "",
+    election_label: "2023年市議会議員一般選挙",
+    elected_count: 1,
+    end_reason_code: "",
+    note: "現職",
+    created_at: "2026-03-25",
+    updated_at: "2026-03-25"
+  },
+  {
+    office_term_id: "OT003",
+    member_id: "M003",
+    term_start_date: "2019-05-01",
+    term_end_date: "2023-04-30",
+    election_label: "2019年市議会議員一般選挙",
+    elected_count: 3,
+    end_reason_code: "TERM_END",
+    note: "任期満了",
+    created_at: "2026-03-25",
+    updated_at: "2026-03-25"
+  }
+];
