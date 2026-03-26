e43fb841f219e2540cd16d4ad733ab52e904d28c
SideNote <166878218+YaMac33@users.noreply.github.com>
2026-03-26 21:01:58 +0900

    Create events.js

---
+++ app/data/events.js
@@ -0,0 +1,34 @@
+window.APP_DATA = window.APP_DATA || {};
+
+window.APP_DATA.events = [
+  {
+    event_id: "EVT001",
+    meeting_id: "MTG2024_01",
+    event_type_id: "PLENARY",
+    committee_id: "",
+    event_date: "2024-02-20",
+    start_time: "10:00",
+    end_time: "12:00",
+    note: "開会"
+  },
+  {
+    event_id: "EVT002",
+    meeting_id: "MTG2024_01",
+    event_type_id: "STANDING_COMMITTEE",
+    committee_id: "C001",
+    event_date: "2024-03-05",
+    start_time: "10:00",
+    end_time: "15:00",
+    note: "総務常任委員会"
+  },
+  {
+    event_id: "EVT003",
+    meeting_id: "MTG2024_01",
+    event_type_id: "STEERING_COMMITTEE",
+    committee_id: "C999",
+    event_date: "2024-02-18",
+    start_time: "13:00",
+    end_time: "14:00",
+    note: "会期前議運"
+  }
+];
