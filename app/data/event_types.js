91a312001d336731e9e67f14bea6895f2892f2b7
SideNote <166878218+YaMac33@users.noreply.github.com>
2026-03-26 21:00:13 +0900

    Create event_types.js

---
+++ app/data/event_types.js
@@ -0,0 +1,22 @@
+window.APP_DATA = window.APP_DATA || {};
+
+window.APP_DATA.event_types = [
+  {
+    event_type_id: "PLENARY",
+    event_type_name: "本会議",
+    sort_order: "1",
+    active_flag: "1"
+  },
+  {
+    event_type_id: "STANDING_COMMITTEE",
+    event_type_name: "常任委員会",
+    sort_order: "2",
+    active_flag: "1"
+  },
+  {
+    event_type_id: "STEERING_COMMITTEE",
+    event_type_name: "議会運営委員会",
+    sort_order: "3",
+    active_flag: "1"
+  }
+];
