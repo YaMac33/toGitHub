62780cd403b2d1a05c608e06529a86941df92e84
SideNote <166878218+YaMac33@users.noreply.github.com>
2026-03-26 22:30:26 +0900

    Update events.js

--- app/data/events.js
+++ app/data/events.js
@@ -4,15 +4,45 @@ window.APP_DATA.events = [
   {
     event_id: "EVT001",
     meeting_id: "MTG2024_01",
+    event_type_id: "STEERING_COMMITTEE",
+    committee_id: "C999",
+    event_date: "2024-02-13",
+    start_time: "13:00",
+    end_time: "14:00",
+    note: "第1回定例会のための事前議運（会期外）"
+  },
+  {
+    event_id: "EVT002",
+    meeting_id: "MTG2024_01",
+    event_type_id: "LEADERS_MEETING",
+    committee_id: "",
+    event_date: "2024-02-15",
+    start_time: "10:00",
+    end_time: "11:00",
+    note: "第1回定例会のための会派代表者会議（会期外）"
+  },
+  {
+    event_id: "EVT003",
+    meeting_id: "MTG2024_01",
     event_type_id: "PLENARY",
     committee_id: "",
     event_date: "2024-02-20",
     start_time: "10:00",
     end_time: "12:00",
-    note: "開会"
+    note: "本会議初日"
   },
   {
-    event_id: "EVT002",
+    event_id: "EVT004",
+    meeting_id: "MTG2024_01",
+    event_type_id: "PLENARY",
+    committee_id: "",
+    event_date: "2024-02-21",
+    start_time: "10:00",
+    end_time: "12:00",
+    note: "本会議2日目"
+  },
+  {
+    event_id: "EVT005",
     meeting_id: "MTG2024_01",
     event_type_id: "STANDING_COMMITTEE",
     committee_id: "C001",
@@ -22,13 +52,43 @@ window.APP_DATA.events = [
     note: "総務常任委員会"
   },
   {
-    event_id: "EVT003",
+    event_id: "EVT006",
     meeting_id: "MTG2024_01",
     event_type_id: "STEERING_COMMITTEE",
     committee_id: "C999",
-    event_date: "2024-02-18",
+    event_date: "2024-03-22",
     start_time: "13:00",
     end_time: "14:00",
-    note: "会期前議運"
+    note: "閉会前議運（会期内）"
+  },
+  {
+    event_id: "EVT007",
+    meeting_id: "MTG2024_02",
+    event_type_id: "STEERING_COMMITTEE",
+    committee_id: "C999",
+    event_date: "2024-05-25",
+    start_time: "13:00",
+    end_time: "14:00",
+    note: "第2回定例会のための事前議運（会期外）"
+  },
+  {
+    event_id: "EVT008",
+    meeting_id: "MTG2024_02",
+    event_type_id: "LEADERS_MEETING",
+    committee_id: "",
+    event_date: "2024-05-28",
+    start_time: "10:00",
+    end_time: "11:00",
+    note: "第2回定例会のための会派代表者会議（会期外）"
+  },
+  {
+    event_id: "EVT009",
+    meeting_id: "MTG2024_02",
+    event_type_id: "PLENARY",
+    committee_id: "",
+    event_date: "2024-06-01",
+    start_time: "10:00",
+    end_time: "12:00",
+    note: "本会議初日"
   }
 ];
