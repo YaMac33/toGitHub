41fc84c128fed4c8a6c170ac06169f3760114c38
SideNote <166878218+YaMac33@users.noreply.github.com>
2026-03-26 21:03:17 +0900

    Create special_committees.js

---
+++ app/data/special_committees.js
@@ -0,0 +1,18 @@
+window.APP_DATA = window.APP_DATA || {};
+
+window.APP_DATA.special_committees = [
+  {
+    special_committee_id: "SC001",
+    special_committee_name: "予算特別委員会",
+    special_committee_type: "BUDGET",
+    sort_order: "1",
+    active_flag: "1"
+  },
+  {
+    special_committee_id: "SC002",
+    special_committee_name: "決算特別委員会",
+    special_committee_type: "SETTLEMENT",
+    sort_order: "2",
+    active_flag: "1"
+  }
+];
