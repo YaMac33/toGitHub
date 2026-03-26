13ef11a2f36e50c634f7af13bf50634045c08867
SideNote <166878218+YaMac33@users.noreply.github.com>
2026-03-26 22:32:25 +0900

    Update special_committees.js

--- app/data/special_committees.js
+++ app/data/special_committees.js
@@ -6,13 +6,23 @@ window.APP_DATA.special_committees = [
     special_committee_name: "予算特別委員会",
     special_committee_type: "BUDGET",
     sort_order: "1",
-    active_flag: "1"
+    active_flag: "1",
+    note: ""
   },
   {
     special_committee_id: "SC002",
     special_committee_name: "決算特別委員会",
     special_committee_type: "SETTLEMENT",
     sort_order: "2",
-    active_flag: "1"
+    active_flag: "1",
+    note: ""
+  },
+  {
+    special_committee_id: "SC003",
+    special_committee_name: "新庁舎整備特別委員会",
+    special_committee_type: "OTHER",
+    sort_order: "3",
+    active_flag: "1",
+    note: ""
   }
 ];
