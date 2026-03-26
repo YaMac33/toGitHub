5fd2a5f87c7e3149dd6de56c44d6d48c357d1e4b
SideNote <166878218+YaMac33@users.noreply.github.com>
2026-03-26 22:35:38 +0900

    Update special_committee_meetings.js

--- app/data/special_committee_meetings.js
+++ app/data/special_committee_meetings.js
@@ -18,5 +18,23 @@ window.APP_DATA.special_committee_meetings = [
     special_committee_instance_id: "SCI001",
     meeting_date: "2024-03-11",
     note: ""
+  },
+  {
+    special_committee_meeting_id: "SCM004",
+    special_committee_instance_id: "SCI001",
+    meeting_date: "2024-03-12",
+    note: ""
+  },
+  {
+    special_committee_meeting_id: "SCM005",
+    special_committee_instance_id: "SCI001",
+    meeting_date: "2024-03-13",
+    note: ""
+  },
+  {
+    special_committee_meeting_id: "SCM006",
+    special_committee_instance_id: "SCI003",
+    meeting_date: "2024-06-03",
+    note: "初回"
   }
 ];
