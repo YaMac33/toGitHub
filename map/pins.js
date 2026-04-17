d91486b2066cdd021ff52af2d16db60890d07c65
YaMac <166878218+YaMac33@users.noreply.github.com>
2026-04-17 21:40:23 +0900

    form update: map/pins.js (2026-04-17 21:40:22)

---
+++ map/pins.js
@@ -0,0 +1,19 @@
+window.APP_DATA = window.APP_DATA || {};
+window.APP_DATA.mapPins = {
+  map_id: "map_001",
+  map_name: "大分市〜北九州市近辺",
+  map_image_file: "map.png",
+  updated_at: "2026-04-17T00:00:00",
+  pins: [
+    {
+      id: "P0001",
+      name: "大分市役所",
+      x: 42.3,
+      y: 61.8,
+      category: "視察先",
+      note: "サンプルデータ",
+      created_at: "2026-04-17T00:00:00",
+      updated_at: "2026-04-17T00:00:00"
+    }
+  ]
+};
