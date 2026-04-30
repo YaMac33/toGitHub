d5183c918cb186d0c55876add32bc8f140d35ab8
Yamac33 <mountflat0104@gmail.com>
2026-04-30 20:07:03 +0900

    Excel対応履歴ビューアのサンプルデータを入力

--- Excel対応履歴ビューア/03_app/data/meta.js
+++ Excel対応履歴ビューア/03_app/data/meta.js
@@ -2,7 +2,7 @@ window.APP_DATA = window.APP_DATA || {};
 window.APP_DATA.meta = {
   app_name: "Excel対応履歴ビューア",
   version: "1.0.0",
-  exported_at: "",
-  case_count: 0,
-  history_count: 0
+  exported_at: "2026-04-30 18:00:00",
+  case_count: 5,
+  history_count: 5
 };
