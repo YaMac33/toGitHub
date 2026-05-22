4d693bcf34718f8368a79cf3e621f05c1478fdd9
Yamac33 <mountflat0104@gmail.com>
2026-05-23 00:23:45 +0900

    GASデプロイ

--- bento-order-app/js/config.example.js
+++ bento-order-app/js/config.example.js
@@ -4,7 +4,7 @@
  * index.html の読み込み先を config.js に変更して利用する想定です。
  */
 const APP_CONFIG = {
-  API_BASE_URL: "",
+  API_BASE_URL: "https://script.google.com/macros/s/AKfycbwWiGOu7LjvHPllRMkt52Wc02NS5YbqD-Ic7q9UkjjSnLfLfocpfYX4IHfwHzPvc1cDpA/exec",
   USE_MOCK_API: true
 };
 
